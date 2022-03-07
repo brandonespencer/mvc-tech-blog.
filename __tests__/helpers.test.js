748d709d1f75dd4bac02125ac9c72faeaa128ab0
Brandon Spencer <46231696+brandonespencer@users.noreply.github.com>
2022-03-07 02:40:18 -0800

    Update helpers.test.js

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -2,9 +2,9 @@ const {format_date} = require('../utils/helpers');
 
 // creating test to that format_date() takes Date() objects and returns dates in MM/DD/YYYY
 test('format_date() returns a date string', () => {
-    const date = new Date('2020-03-20 16:12:03');
+    const date = new Date('2022-03-7 16:12:03');
   
-    expect(format_date(date)).toBe('3/20/2020');
+    expect(format_date(date)).toBe('3/7/2022');
   });
 
 const {format_plural} = require('../utils/helpers')
